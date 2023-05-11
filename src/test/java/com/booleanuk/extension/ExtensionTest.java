3548484d91ff397ffeab9ac9d6320392cbd7fb59
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-05-11 21:11:51 +0300

    Extension test added

---
+++ src/test/java/com/booleanuk/extension/ExtensionTest.java
@@ -0,0 +1,12 @@
+package com.booleanuk.extension;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExtensionTest {
+
+    @Test
+    public void testA() {
+        Assertions.assertTrue(true);
+    }
+}
